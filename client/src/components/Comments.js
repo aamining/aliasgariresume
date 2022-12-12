717d73df012fa3fa2d5c53a5155ddd5cc50187ad
ali asgari <aamining@gmail.com>
2022-12-12 18:08:11 +1100

    try to push heroku 41

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -26,7 +26,7 @@ class Comments extends Component{
 
             await axios.request({
                 method:'get',
-                url:'http://localhost:7000/comments',
+                url:'/api/comments',
                 headers: {"Authorization": `Bearer ${this.state.token}`}
 
             })
